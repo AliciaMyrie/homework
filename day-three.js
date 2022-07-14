4cfc4aaa481c249531024358a5548bff1da3af65
Alicia Myrie <aliciamyrie@Alicias-MacBook-Air.local>
2022-07-14 17:07:43 -0400

    added

---
+++ day-three.js
@@ -0,0 +1,29 @@
+//1. Write a function that adds two numbers together.
+//2. Write a car object that has a modal and color.  
+//Create a property called year then console.log all of the objects properties individually.
+//3. Write a function to get the first element in an array.
+
+function numTotal(x, y) {
+return x + y 
+}
+console.log(numTotal(3,7));
+
+let car = {
+    model: "Mazda,",
+    color: "white",
+};
+
+car.year = 2007
+
+console.log(car)
+console.log(car.year)
+console.log(car.model)
+console.log(car.color)
+
+//Question 3
+
+// function firstElement(array){
+//     console.log(array[0])
+// }
+// let fruit =["kiwi", "plum", "tamarind", "apple", "mango"]
+// console.log(fruit[0])
